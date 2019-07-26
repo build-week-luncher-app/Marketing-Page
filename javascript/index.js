9e303e4ce6f648d5349a66f369edfac4115fc7a6
durbcodes <durbcodes@gmail.com>
2019-07-25 21:06:06 -0400

    Found an outside source for icons. Footer and meet cards are complete. Hero image with transitions is set for About page.

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,9 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
+
 let swiper = new Swiper('.swiper-container', {
   effect: 'coverflow',
   grabCursor: true,
@@ -6,11 +12,32 @@ let swiper = new Swiper('.swiper-container', {
   coverflowEffect: {
     rotate: 60,
     stretch: 0,
-    depth: 300,
-    modifier: 2,
+    depth: 400,
+    modifier: 1,
     slideShadows : true,
   },
   pagination: {
     el: '.swiper-pagination',
   },
 });
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "100%" },
+    { width: "80%", ease: Power2.easeInOut }
+  )
+  .fromTo(
+    slider,
+    1.2,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2"
+  )
+  .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(hamburger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
+
