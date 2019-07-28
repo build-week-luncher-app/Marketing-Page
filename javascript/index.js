d1c4d9dc025a1f986877515df5b8f81d9df1d13c
durbcodes <durbcodes@gmail.com>
2019-07-28 16:35:42 -0400

    Added the Bulma framework for css and applied changes to the page and nav to help with the added library.

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
 const logo = document.querySelector('#logo');
+const nav = document.querySelector('.level');
 const headline = document.querySelector('.headline');
 
 let swiper = new Swiper('.swiper-container', {
@@ -22,19 +23,13 @@ let swiper = new Swiper('.swiper-container', {
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-  .fromTo(
-    hero,
-    1.2,
-    { width: "100%" },
-    { width: "90%", ease: Power2.easeInOut }
-  )
-  .fromTo(
-    slider,
-    1.2,
-    { x: "-100%" },
-    { x: "0%", ease: Power2.easeInOut },
-    "-=1.2"
-  )
-  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.2");
+tl.fromTo(hero, 1, { height: "0%" }, { height: "90%", ease: Power2.easeInOut })
+  .fromTo(hero, 1.2, { width: "100%" }, { width: "90%", ease: Power2.easeInOut })
+  .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(nav, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3")
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3");
 
+tl.fromTo(hero, 2, { width: "90%" }, { width: "100%", ease: Power2.easeInOut }, "-=5");
+
+  
+  
