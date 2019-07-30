7c048b966b46e41ac52005babf7f6bd6493a00b5
durbcodes <durbcodes@gmail.com>
2019-07-29 20:20:16 -0400

    Added some comments.

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,17 @@ const logo = document.querySelector('#logo');
 const nav = document.querySelector('.level');
 const headline = document.querySelector('.headline');
 
+// Animations for the header on the about page using Tween and Timeline
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, { height: "0%" }, { height: "90%", ease: Power2.easeInOut })
+  .fromTo(hero, 1.2, { width: "100%" }, { width: "90%", ease: Power2.easeInOut })
+  .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(nav, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3")
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3");
+
+
+// Animation from swiper.js
 let swiper = new Swiper('.swiper-container', {
   effect: 'coverflow',
   grabCursor: true,
@@ -20,11 +31,3 @@ let swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
   },
 });
-
-const tl = new TimelineMax();
-
-tl.fromTo(hero, 1, { height: "0%" }, { height: "90%", ease: Power2.easeInOut })
-  .fromTo(hero, 1.2, { width: "100%" }, { width: "90%", ease: Power2.easeInOut })
-  .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut },"-=1.2")
-  .fromTo(nav, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3")
-  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3");
