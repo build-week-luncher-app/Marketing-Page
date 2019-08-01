18fd6c391727aceccfe18d9e83cdbd30a9bd3414
Kyle Leonard <kdleonard@icloud.com>
2019-07-31 20:02:31 -0500

    Adding new animation
    Need to figure out error

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
+const p = document.querySelector('.p');
+
+
+const tween = new TweenMax();
+
+tween.fromTo(p, 1, {x:0}, {x:100});
+
 window.onload = function () {
     
-    /* event listener */
+    /* event listener for submit button*/
     document.getElementsByName("submit")[0].addEventListener('click', doThing);
     
-    /* function */
+
     function doThing(){
        alert('Thank you for sharing!');
     }
